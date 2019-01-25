871cf936f2336160343fcf76ab1431eeeb3fd073
ziyadmerhebi <ziyadmerhebi@gmail.com>
2019-01-24 19:24:06 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const { Users, CurrencyShop } = require('./dbObjects');
+const { Op } = require('sequelize');
+const currency = new Discord.Collection();
+const PREFIX = '!';
+
+// [alpha]
+Reflect.defineProperty(currency, 'add', {
+	value: async function add(id, amount) {
+		const user = currency.get(id);
+		if (user) {
+			user.balance += Number(amount);
+			return user.save();
+		}
+		const newUser = await Users.create({ user_id: id, balance: amount });
+		currency.set(id, newUser);
+		return newUser;
+	},
+});
+
+Reflect.defineProperty(currency, 'getBalance', {
+	value: function getBalance(id) {
+		const user = currency.get(id);
+		return user ? user.balance : 0;
+	},
+});
+
+client.once('ready', async () => {
+	// [beta]
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+	currency.add(message.author.id, 1);
+
+	if (!message.content.startsWith(PREFIX)) return;
+	const input = message.content.slice(PREFIX.length).trim();
+	if (!input.length) return;
+	const [, command, commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
+
+	if (command === 'balance') {
+		// [gamma]
+	} else if (command === 'inventory') {
+		// [delta]
+	} else if (command === 'transfer') {
+		// [epsilon]
+	} else if (command === 'buy') {
+		// [zeta]
+	} else if (command === 'shop') {
+		// [theta]
+	} else if (command === 'leaderboard') {
+		// [lambda]
+	}
+});
+
+client.login('pleaseinsertyourtokenheresothistutorialcanwork');
